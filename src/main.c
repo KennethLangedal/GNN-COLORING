03dbe58d5648c2cd5627aeeefa3cfb3081459c2b
KennethLangedal <kenneth.langedal@hotmail.com>
2024-04-24 19:40:56 +0200

    Fast option

--- src/main.c
+++ src/main.c
@@ -32,12 +32,12 @@ int test_ordering(uint32_t N, uint32_t *V, uint32_t *E,
 int main(int argc, char **argv)
 {
     char *graph_path = "";
-    int p = 0, s = 0, k = 1, t = 1, v = 0;
+    int p = 0, s = 0, f = 0, k = 1, t = 1, v = 0;
 
     int c;
     opterr = 0;
 
-    while ((c = getopt(argc, argv, "g:pck:t:vh")) != -1)
+    while ((c = getopt(argc, argv, "g:pcfk:t:vh")) != -1)
     {
         switch (c)
         {
@@ -49,6 +49,9 @@ int main(int argc, char **argv)
             break;
         case 'c':
             s = 1;
+            break;
+        case 'f':
+            f = 1;
             break;
         case 'k':
             k = atoi(optarg);
@@ -66,6 +69,7 @@ int main(int argc, char **argv)
             printf("-g path, sets input graph file\n");
             printf("-p, run parallel experiments\n");
             printf("-c, compute colors used fast\n");
+            printf("-f, only do FF, LF, SL, and GNN\n");
             printf("-k #, number of repeated colorings\n");
             printf("-t #, total number of threads for faster I/O and validation\n");
             printf("-v, verbose\n");
@@ -87,14 +91,14 @@ int main(int argc, char **argv)
 
     double t0 = omp_get_wtime();
     uint32_t N, *V, *E;
-    FILE *f = fopen(graph_path, "r");
-    if (f == NULL)
+    FILE *fp = fopen(graph_path, "r");
+    if (fp == NULL)
     {
         printf("Unable to open file %s\n", graph_path);
         return 1;
     }
-    mtx_parse_par(f, &N, &V, &E);
-    fclose(f);
+    mtx_parse_par(fp, &N, &V, &E);
+    fclose(fp);
     double t1 = omp_get_wtime(), t2;
 
     if (v)
@@ -120,7 +124,8 @@ int main(int argc, char **argv)
         double *p = aligned_alloc(32, sizeof(double) * (N + 8));
         int *color = malloc(sizeof(int) * N);
 
-        int ff, lf, llf, sl, sll, id, sda, sd, gnn2, gnn3, gnn4;
+        int ff = 0, lf = 0, llf = 0, sl = 0, sll = 0,
+            id = 0, sda = 0, sd = 0, gnn2 = 0, gnn3 = 0, gnn4 = 0;
 
         first_fit_ordering_par(N, V, E, p);
         ff = test_ordering(N, V, E, jp, p, color, k);
@@ -128,14 +133,8 @@ int main(int argc, char **argv)
         largest_degree_first_ordering(N, V, E, p);
         lf = test_ordering(N, V, E, jp, p, color, k);
 
-        largest_log_degree_first_ordering(N, V, E, p);
-        llf = test_ordering(N, V, E, jp, p, color, k);
-
         smallest_degree_last_ordering(N, V, E, p);
         sl = test_ordering(N, V, E, jp, p, color, k);
-
-        smallest_log_degree_last_ordering(N, V, E, p);
-        sll = test_ordering(N, V, E, jp, p, color, k);
 
         gnn_ordering(N, V, E, p, 2);
         gnn2 = test_ordering(N, V, E, jp, p, color, k);
@@ -146,14 +145,23 @@ int main(int argc, char **argv)
         gnn_ordering(N, V, E, p, 4);
         gnn4 = test_ordering(N, V, E, jp, p, color, k);
 
-        incidence_degree_ordering(N, V, E, p);
-        id = test_ordering(N, V, E, jp, p, color, k);
+        if (!f)
+        {
+            largest_log_degree_first_ordering(N, V, E, p);
+            llf = test_ordering(N, V, E, jp, p, color, k);
 
-        saturation_degree_ordering_alt(N, V, E, p);
-        sda = test_ordering(N, V, E, jp, p, color, k);
+            smallest_log_degree_last_ordering(N, V, E, p);
+            sll = test_ordering(N, V, E, jp, p, color, k);
 
-        saturation_degree_ordering(N, V, E, p);
-        sd = test_ordering(N, V, E, jp, p, color, k);
+            incidence_degree_ordering(N, V, E, p);
+            id = test_ordering(N, V, E, jp, p, color, k);
+
+            saturation_degree_ordering_alt(N, V, E, p);
+            sda = test_ordering(N, V, E, jp, p, color, k);
+
+            saturation_degree_ordering(N, V, E, p);
+            sd = test_ordering(N, V, E, jp, p, color, k);
+        }
 
         if (v)
             printf("ff lf llf sl sll id sda sd gnn2 gnn3 gnn4\n");
@@ -176,9 +184,11 @@ int main(int argc, char **argv)
         }
         first_fit_runner(N, V, E, 5, k, t, ntc, nt);
         largest_degree_first_runner(N, V, E, 5, k, t, ntc, nt);
-        largest_log_degree_first_runner(N, V, E, 5, k, t, ntc, nt);
+        if (!f)
+            largest_log_degree_first_runner(N, V, E, 5, k, t, ntc, nt);
         smallest_degree_last_runner(N, V, E, 5, k, t, ntc, nt);
-        smallest_log_degree_last_runner(N, V, E, 5, k, t, ntc, nt);
+        if (!f)
+            smallest_log_degree_last_runner(N, V, E, 5, k, t, ntc, nt);
         gnn_runner(N, V, E, 2, 5, k, t, ntc, nt);
         gnn_runner(N, V, E, 3, 5, k, t, ntc, nt);
         gnn_runner(N, V, E, 4, 5, k, t, ntc, nt);
